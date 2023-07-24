42bff3a0af82c6739bc94759a335d9a4c90ae62d
giuseppedch <109151053+giuseppedch@users.noreply.github.com>
2023-07-24 10:09:18 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSaGubm3kkKw4ge_duJx3X-yAr_WTxAbE_mS0_wOcTyD8mK79PPpTnUhrcie5KZ7zaiQDrX6r3KShJr/pubhtml';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
